d1644e5a58fb4f253cc0094aca42f1ed048ce5b2
allanmaeda <allanmaeda@gmail.com>
2021-02-06 15:10:26 -0300

    Add theme dark and light. Add sessionStorage do save state of theme.

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,10 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#333',
+        secundary: '#765195',
+        background: '#222',
+        text: '#fff'
+    },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#e3e3e3',
+        secundary: '#d63af9',
+        background: '#e7e7e7',
+        text: '#333'
+    },
+};
